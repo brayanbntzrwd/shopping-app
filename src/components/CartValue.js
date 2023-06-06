a47e1c72bf8c58996701aae209319c964372fb73
brayanbntzrwd <brayanbntzrwd@gamil.com>
2023-06-05 18:32:18 -0600

    project completed

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,7 +1,18 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
+// Component that displays the total price of all selected items
 const CartValue = () => {
+	const {expenses, Location} = useContext(AppContext);
+	const totalExpenses = expenses.reduce((total, item)=>{
+		return (total += (item.unitprice* item.quantity));
+	}, 0);
+
+	return(
+		<div className='alert alert-primary'>
+			<span>CartValue: {Location}{totalExpenses}</span>
+		</div>
+	);
 };
 
 export default CartValue;
